e70755ebd64d23273d76b01c5e6ce333dc39a28e
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-07-15 20:15:31 -0700

    fix alpha onn d4 gif and added all 12 sides

--- screens/DFourScreen.js
+++ screens/DFourScreen.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Shake } from "react-native-motion";
 
 const DFourScreen = () => {
   const [dFour, setDFour] = useState(require("../assets/dice/d4/d4_face3.png"));
+  const [prevNum, setPrevNum] = useState(0);
+  const [gifTwo, setGifTwo] = useState(
+    require("../assets/dice/d4/4d_face_anim2.gif")
+  );
+
+  // const prevRNumRef = useRef();
+  // useEffect(() => {
+  //   prevRNumRef.current = rNumber;
+  // }, []);
+
+  // const prevRNum = prevRNumRef.current;
+  // console.log(prevRNum);
 
   const getRandomValue = () => {
     //add 1 to random math so you don't get a zero
-    return Math.floor(Math.random() * 4) + 1;
+    return Math.floor(Math.random() * 12) + 1;
   };
+
+  let rNumber = getRandomValue();
+
+  // console.log(prevNum);
 
   // const randomFace = () => {
   //   let rNumber = getRandomValue();
@@ -31,60 +54,132 @@ const DFourScreen = () => {
   // };
 
   const rollButtonPressed = () => {
+    if (rNumber === prevNum) {
+      const sameNums = () => {
+        Alert.alert("You Rolled The Same Number", "Please Continue");
+        // setDFour(require("../assets/dice/d4/d4_face2.png"));
+        // setPrevNum(prevNum + 1);
+        // console.log("did I do anything 1");
+        // console.log(`same numbers inside ${prevNum}`);
+      };
+      sameNums();
+      // alert("rolled same number");
+      // setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
+      // // setDFour(require("../assets/dice/d4/d4_face2.png"));
+
+      // rollButtonPressed();
+      // setDFour(require("../assets/dice/d4/d4_face2.png"));
+      // setPrevNum(prevNum + 1);
+
+      // return setDFour(rNumber);
+    }
     setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
+    // if (rNumber === prevNum) {
+    //   // alert("rolled same number");
+    //   // setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
+    //   // setDFour(require("../assets/dice/d4/d4_face2.png"));
+    //   setDFour(rNumber);
+    //   setPrevNum(prevNum + 1);
+    // }
     // setTimeout(() => {
     //   setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
     // }, 300);
     // randomFace();
+    // if (rNumber === prevNum) {
+    //   // alert("rolled same number");
+    //   setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
+    //   // setDFour(require("../assets/dice/d4/d4_face2.png"));
+    //   setDFour(rNumber);
+    //   setPrevNum(prevNum + 1);
+    // }
+
     let promise1 = new Promise(function (resolve, reject) {
       setTimeout(() => {
-        console.log("2second");
+        // if (rNumber === prevNum) {
+        //   // alert("rolled same number");
+        //   // setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
+        //   // // setDFour(require("../assets/dice/d4/d4_face2.png"));
+
+        //   // rollButtonPressed();
+        //   return setDFour(rNumber);
+        //   // setPrevNum(prevNum + 1);
+        //   // return setDFour(rNumber);
+        // }
+
         // setDFour(require("../assets/dice/d4/4d_face_anim.gif"));
         resolve();
-      }, 700);
+      }, 1800);
     });
-
-    // let promise2 = new Promise(function (resolve, reject) {
-    //   setTimeout(function b() {
-    //     alert("Hello B");
-    //     resolve();
-    //   }, 3000);
-    // });
-
-    // let promise3 = new Promise(function (resolve, reject) {
-    //   setTimeout(function c() {
-    //     alert("Hello C");
-    //     resolve();
-    //   }, 3000);
-    // });
 
     Promise.all([promise1]).then(function () {
       function d() {
-        let rNumber = getRandomValue();
+        // let rNumber = getRandomValue();
 
-        switch (rNumber) {
-          case 1:
-            setDFour(require("../assets/dice/d4/d4_face1.png"));
-            break;
-          case 2:
-            setDFour(require("../assets/dice/d4/d4_face2.png"));
-            break;
-          case 3:
-            setDFour(require("../assets/dice/d4/d4_face3.png"));
-            break;
-          case 4:
-            setDFour(require("../assets/dice/d4/d4_face4.png"));
-            break;
-          default:
-            if (rNumber === rNumber) {
-              setDFour(require("../assets/dice/d4/d4_face1.png"));
-            }
-        }
-        console.log(rNumber);
+        randomFace();
+
+        // switch (rNumber) {
+        //   case 1:
+        //     setDFour(require("../assets/dice/d4/d4_face1.png"));
+        //     break;
+        //   case 2:
+        //     setDFour(require("../assets/dice/d4/d4_face2.png"));
+        //     break;
+        //   case 3:
+        //     setDFour(require("../assets/dice/d4/d4_face3.png"));
+        //     break;
+        //   case 4:
+        //     setDFour(require("../assets/dice/d4/d4_face4.png"));
+        //     break;
+        // }
       }
       d();
     });
   };
+
+  const randomFace = () => {
+    switch (rNumber) {
+      case 1:
+        setDFour(require("../assets/dice/d4/d4_face1_1_rev.png"));
+        break;
+      case 2:
+        setDFour(require("../assets/dice/d4/d4_face2_1_rev.png"));
+        break;
+      case 3:
+        setDFour(require("../assets/dice/d4/d4_face3_1_rev.png"));
+        break;
+      case 4:
+        setDFour(require("../assets/dice/d4/d4_face4_2_rev.png"));
+        break;
+      case 5:
+        setDFour(require("../assets/dice/d4/d4_face1_2_rev.png"));
+        break;
+      case 6:
+        setDFour(require("../assets/dice/d4/d4_face2_2_rev.png"));
+        break;
+      case 7:
+        setDFour(require("../assets/dice/d4/d4_face3_3_rev.png"));
+        break;
+      case 8:
+        setDFour(require("../assets/dice/d4/d4_face4_3_rev.png"));
+        break;
+      case 9:
+        setDFour(require("../assets/dice/d4/d4_face1_4_rev.png"));
+        break;
+      case 10:
+        setDFour(require("../assets/dice/d4/d4_face2_3_rev.png"));
+        break;
+      case 11:
+        setDFour(require("../assets/dice/d4/d4_face3_4_rev.png"));
+        break;
+      case 12:
+        setDFour(require("../assets/dice/d4/d4_face4_4_rev.png"));
+        break;
+    }
+
+    setPrevNum(rNumber);
+  };
+
+  // console.log(`step 1 the prevNum r p ${rNumber} ${prevNum}`);
 
   return (
     <View
@@ -121,8 +216,8 @@ const styles = StyleSheet.create({
   dice: {
     zIndex: 1000,
     elevation: 1000,
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
     marginBottom: 50,
   },
   gamebutton: {
